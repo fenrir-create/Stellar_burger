d730a61b926f9614e35a5652cf4e32f1431671c9
fenrir-create <tocfedor@gmail.com>
2025-06-11 12:57:09 +0300

    77

--- cypress/e2e/constructorBurger.cy.ts
+++ cypress/e2e/constructorBurger.cy.ts
@@ -21,28 +21,37 @@ context('Ингредиенты и конструктор', () => {
   it('Добавление начинки увеличивает счётчик', () => {
     cy.get(SELECTORS.filling).find('button').click();
     cy.get(SELECTORS.filling).find('.counter__num').should('have.text', '1');
+    cy.get(SELECTORS.constructor).should('contain.text', 'марсианской Магнолии');
   });
 
   it('Можно собрать бургер с булкой и начинкой', () => {
     cy.get(SELECTORS.bun).find('button').click();
     cy.get(SELECTORS.filling).find('button').click();
+    cy.get(SELECTORS.constructor).should('contain.text', 'верх');
+    cy.get(SELECTORS.constructor).should('contain.text', 'низ');
     cy.get(SELECTORS.orderBtn).should('not.be.disabled');
   });
 
   it('Можно сначала положить начинку, а потом булку', () => {
     cy.get(SELECTORS.filling).find('button').click();
     cy.get(SELECTORS.bun).find('button').click();
+    cy.get(SELECTORS.constructor).should('contain.text', 'марсианской Магнолии');
   });
 
   it('Замена булки без начинки работает корректно', () => {
     cy.get(SELECTORS.bun).find('button').click();
     cy.get(SELECTORS.altBun).find('button').click();
+    cy.get(SELECTORS.constructor).should('contain.text', 'верх');
+    cy.get(SELECTORS.constructor).should('contain.text', 'низ');
   });
 
   it('Замена булки при наличии начинки работает', () => {
     cy.get(SELECTORS.bun).find('button').click();
     cy.get(SELECTORS.filling).find('button').click();
     cy.get(SELECTORS.altBun).find('button').click();
+    cy.get(SELECTORS.constructor).should('contain.text', 'верх');
+    cy.get(SELECTORS.constructor).should('contain.text', 'низ');
+    cy.get(SELECTORS.constructor).should('contain.text', 'марсианской Магнолии');
   });
 });
 
@@ -63,6 +72,12 @@ context('Оформление заказа', () => {
     cy.get(SELECTORS.orderBtn).click();
     cy.wait('@postOrder');
     cy.get('@modalContainer').find('h2').should('contain.text', '888999');
+    cy.get('@modalContainer').find('button').click(); // закрыть модалку
+    cy.get('@modalContainer').should('be.empty');
+
+    // Проверка, что конструктор очистился
+    cy.get(SELECTORS.constructor).should('contain.text', 'Выберите булки');
+    cy.get(SELECTORS.constructor).should('contain.text', 'Выберите начинку');
   });
 });
 
@@ -72,22 +87,26 @@ context('Модальные окна ингредиентов', () => {
     cy.get(SELECTORS.filling).find('a').click();
     cy.get('@modalContainer').should('not.be.empty');
     cy.url().should('include', '643d69a5c3f7b9001cfa0941');
+    cy.get('@modalContainer').should('contain.text', 'Биокотлета из марсианской Магнолии');
   });
 
   it('Закрытие модального окна через кнопку ✕', () => {
     cy.get(SELECTORS.filling).find('a').click();
+    cy.get('@modalContainer').should('not.be.empty');
     cy.get('@modalContainer').find('button').click();
     cy.get('@modalContainer').should('be.empty');
   });
 
   it('Закрытие модалки по клику на оверлей', () => {
     cy.get(SELECTORS.filling).find('a').click();
+    cy.get('@modalContainer').should('not.be.empty');
     cy.get(SELECTORS.overlay).click({ force: true });
     cy.get('@modalContainer').should('be.empty');
   });
 
   it('Закрытие по клавише Escape', () => {
     cy.get(SELECTORS.filling).find('a').click();
+    cy.get('@modalContainer').should('not.be.empty');
     cy.get('body').type('{esc}');
     cy.get('@modalContainer').should('be.empty');
   });
